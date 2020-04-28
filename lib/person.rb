f9c3a638732b8780c08400c0ef16ede42e59ae99
Christopher Lewis <cjoellewis@yahoo.com>
2020-04-28 22:36:57 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,16 @@
 class Person
   
-  def 
+  def name=(first_name)
+    @name = first_name
+    @name = Beyonce
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(occupation)
+    @job = occupation
+    @job = singer
+  end
+end
