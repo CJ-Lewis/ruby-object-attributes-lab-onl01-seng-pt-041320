fbd09d539d797a6789524dbf76e650d3ee20e7f5
Christopher Lewis <cjoellewis@yahoo.com>
2020-04-28 22:33:40 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  
+  def name(dogs_name)
+    dogs_name = @name
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed=(dog_breed)
+    dog_breed = @breed
+  end
+  
+  def breed
+    dog_breed = @breed
+  end
+end
